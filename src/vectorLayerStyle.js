cf83c0542e31de0fcc8d26aea8ff1f8eda7b1225
simonseyock <simonseyock@googlemail.com>
2017-03-24 00:46:30 +0100

    geometrycollection styles

--- src/vectorLayerStyle.js
+++ src/vectorLayerStyle.js
@@ -1,4 +1,4 @@
-var style = new ol.style.Style({
+const pointStyle = new ol.style.Style({
   image: new ol.style.Circle({
     radius: 7.5,
     stroke: new ol.style.Stroke({
@@ -12,53 +12,81 @@ var style = new ol.style.Style({
     textBaseline: 'middle',
     offsetX: 10
   }),
+  zIndex: 2
+});
+
+const lineStyle = new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 7.5,
+    stroke: new ol.style.Stroke({
+      color: 'purple',
+      width: 2
+    })
+  }),
   stroke: new ol.style.Stroke({
     color: 'purple',
     width : 4
-    })
+  }),
+  zIndex: 1
 });
+
+function createGeometryStyle(feature, resolution, geometry) {
+  if (geometry instanceof ol.geom.Point) {
+    let style = pointStyle.clone();
+    style.setGeometry(geometry);
+
+    if (feature.get('icon')) {
+      switch (feature.get('icon')) {
+        case 'test':
+          style.setImage(new ol.style.Circle({
+            radius: 7.5,
+            stroke: new ol.style.Stroke({
+              color: 'yellow',
+              width: 2
+            })
+          }));
+          break;
+        case 'event':
+          style.setImage(new ol.style.Circle({
+            radius: 7.5,
+            stroke: new ol.style.Stroke({
+              color: 'purple',
+              width: 2
+            })
+          }));
+          break;
+        case 'house':
+          style.setImage(new ol.style.Icon({
+            anchor: [0.5, 1],
+            src: '../icons/house.png'
+          }));
+          break;
+      }
+    }
+
+    if (resolution < 39) {
+      style.getText().setText(feature.get('name'));
+    }
+
+    return style;
+  }
+  else if (geometry instanceof ol.geom.LineString) {
+    let style = lineStyle.clone();
+    style.setGeometry(geometry);
+    return style;
+  }
+}
 
 export function vectorLayerStyle(feature, resolution) {
   if (feature.get('hidden')) {
     return null;
   }
 
-  var featureStyle = style.clone();
-
-  if (feature.get('icon')) {
-    switch (feature.get('icon')) {
-      case 'test':
-        featureStyle.setImage(new ol.style.Circle({
-          radius: 7.5,
-          stroke: new ol.style.Stroke({
-            color: 'yellow',
-            width: 2
-          })
-        }));
-        break;
-      case 'event':
-        featureStyle.setImage(new ol.style.Circle({
-          radius: 7.5,
-          stroke: new ol.style.Stroke({
-            color: 'purple',
-            width: 2
-          })
-        }));
-        break;
-      case 'house':
-        featureStyle.setImage(new ol.style.Icon({
-          anchor: [0.5, 1],
-            src: '../icons/house.png'
-          }));
-        break;            
-    }
-  }
-
-  if(resolution < 39) {
-    featureStyle.getText().setText(feature.get('name'));
-    return featureStyle;
+  let geom = feature.getGeometry();
+  if (geom instanceof ol.geom.GeometryCollection) {
+    return geom.getGeometries().map(g => createGeometryStyle(feature, resolution, g));
   }
   else {
-    return featureStyle;
+    return createGeometryStyle(feature, resolution, geom);
   }
 }
