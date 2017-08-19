d230c77e1c3b4359b335936377c2dfb6801c56e5
Simon Seyock <simonseyock@googlemail.com>
2017-08-19 13:05:53 +0200

    reorganised feature datails

--- src/map.js
+++ src/map.js
@@ -1,12 +1,13 @@
 import {baseLayers} from "./baseLayers";
 import {TimePicker} from "./time-picker/time-picker";
-import {featureDetails} from "./featureDetails";
+import {featureDetails} from "./feature-details";
 import {panelHide} from "./panelHide";
 import {vectorLayers} from "./vectorLayers";
+import {FeatureDetails} from "./feature-details";
 
 export function createMap() {
   // proj4.defs("EPSG:31466", "+proj=tmerc +lat_0=0 +lon_0=6 +k=1 +x_0=2500000 +y_0=0 +ellps=bessel +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs");
-  
+
   let map = new ol.Map({
     target: 'map',
     controls: ol.control.defaults({
@@ -23,7 +24,7 @@ export function createMap() {
   baseLayers(map);
   let vectorLayers_ = vectorLayers(map);
   let timePicker = new TimePicker('#footer', 'data/time-line.json', vectorLayers_);
-  featureDetails(map, vectorLayers_, timePicker);
+  let features = new FeatureDetails(map, vectorLayers_, timePicker);
 
   panelHide(map);
 
