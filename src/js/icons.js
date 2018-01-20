eb415d5378170fb1374b6e5aa02c629646dd35f5
robin-tunger <robin.tunger@yahoo.de>
2018-01-20 18:39:38 +0100

    plus wesel

--- src/js/icons.js
+++ src/js/icons.js
@@ -1,14 +1,14 @@
 export const ICONS = {
-  'flagw': '../images/flagw.png',
-  'house': '../images/house.png',
-  'peace': '../images/peace.png',
-  'sfb': '../images/sfb.png',
-  'theater': '../images/theater.png',
-  'info': '../images/info.png',
-  'shakehand': '../images/shakehand.png',
-  'camp': '../images/camp.png',
-  'strike': '../images/strike.png',
-  'meet': '../images/meet.png',
-  'sfbmeet': '../images/sfbmeet.png'
-    
+    'flagw': '../images/flagw.png',
+    'house': '../images/house.png',
+    'peace': '../images/peace.png',
+    'sfb': '../images/sfb.png',
+    'theater': '../images/theater.png',
+    'info': '../images/info.png',
+    'shakehand': '../images/shakehand.png',
+    'camp': '../images/camp.png',
+    'strike': '../images/strike.png',
+    'meet': '../images/meet.png',
+    'sfbmeet': '../images/sfbmeet.png',
+    'grave': '../images/grave.png'
 };
